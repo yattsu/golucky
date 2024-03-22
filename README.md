e7dfa3d95dd337576224327a9836491b62bad01a
yattsu <yattsu@users.noreply.github.com>
2024-03-22 20:41:20 +0200

    The end.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Golucky
+
+This is a game about a cat I hold dear.
+
+### Look how cute it is
+
+<p align="center">
+  <img src="ender.jpg" />
+</p>
+
+# Play
+1. `git clone https://github.com/yattsu/golucky`
+2. `go mod download`
+3. `go run main.go`
+
--- main.go
+++ main.go
@@ -4,21 +4,48 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"crypto/sha256"
 	"fmt"
 	"io"
 	"log"
+	mathrand "math/rand/v2"
 	"os"
+	"os/exec"
+	"reflect"
 	"runtime"
 )
 
+var clear map[string]func()
+
 func init() {
 	log.SetFlags(0)
+
+	clear = make(map[string]func())
+	clear["linux"] = func() {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
 }
 
 func main() {
-	filePath, err := pathToDummy()
+	callClear()
+
+	lives := 253
+	filePath, err := pathToCat()
 	if err != nil {
-		log.Panicln("Could not find file.")
+		log.Println("A cat is not present...")
+		os.Exit(0)
+	}
+	if !checkSum() {
+		log.Println("The cat...")
+		log.Println("...it's...dust.")
+		os.Exit(0)
 	}
 
 	key := make([]byte, 32)
@@ -26,24 +53,105 @@ func main() {
 		log.Panicln("Could not generate volatile.")
 	}
 
-	encryptFile(key, filePath)
+	if err := encryptFile(key, filePath); err != nil {
+		log.Panicln("Could not encrypt cat.")
+	}
+
+	log.Println("The cat's soul is jumbled (by AES256 encryption...)")
+	log.Println("Restore its mangled soul by winning the slots")
+	log.Println("Closing this program will cause the cat to cease forever")
+	log.Println(fmt.Sprintf("You have %d lives left", lives))
 	var pause string
-	fmt.Scan(&pause)
-	decryptFile(key, filePath)
+	log.Println("\nPress Enter to spin.")
+	fmt.Scanf("\n", &pause)
+
+	win := slots(lives)
+
+	if win {
+		decryptFile(key, filePath)
+		log.Println("\nGambled a cat's soul and won")
+		log.Println("It is safe...")
+		log.Println("               ...for now.")
+		os.Exit(0)
+	} else {
+		callClear()
+		log.Println("The cat is forever lost...")
+		log.Println("         ...it is only a remnant of what once was...")
+		log.Println("                  ...forever in your memory.")
+		os.Exit(0)
+	}
 }
 
-func pathToDummy() (string, error) {
-	fileName := "cat.jpg"
+func callClear() {
+	value, ok := clear[runtime.GOOS]
+	if ok {
+		value()
+	}
+}
+
+func checkSum() bool {
+	const sha = "c732789e3469f68d210e6fba42839a3f61420e71ffcfc606f385d6de7d8d926c"
+	var match bool
+	match = false
+
+	f, err := os.Open("ender.jpg")
+	if err != nil {
+		log.Panicln("Could not check the cat's soul.")
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		log.Panicln("Could not check the cat's soul.")
+	}
+
+	if fmt.Sprintf("%x", h.Sum(nil)) == sha {
+		match = true
+	}
+
+	return match
+}
+
+func slots(lives int) bool {
+	results := make([]int, 3)
+	win := false
+
+	var pause string
+	for {
+		callClear()
+		for idx := range results {
+			results[idx] = mathrand.IntN(10)
+		}
+		lives -= 1
+		log.Println("_ - _ - _ - _")
+		log.Println(fmt.Sprintf("| %d | %d | %d |", results[0], results[1], results[2]))
+		log.Println("= = = = = = =")
+		log.Println(fmt.Sprintf("   ❤️ %d ❤️", lives))
+		if reflect.DeepEqual(results, []int{4, 4, 4}) {
+			win = true
+			break
+		}
+		if lives == 0 {
+			log.Println("\nOh no...")
+		} else {
+			log.Println("\nPress Enter to spin.")
+		}
+		fmt.Scanf("\n", &pause)
+		if lives <= 0 {
+			break
+		}
+	}
+
+	return win
+}
+
+func pathToCat() (string, error) {
+	fileName := "ender.jpg"
 	var path string
 	var err error
 
-	switch runtime.GOOS {
-	case "linux":
-		if _, err = os.Stat(fmt.Sprintf("/tmp/%s", fileName)); err == nil {
-			path = fmt.Sprintf("/tmp/%s", fileName)
-		}
-	default:
-		path = ""
+	if _, err = os.Stat(fileName); err == nil {
+		path = fileName
 	}
 
 	return path, err
@@ -58,7 +166,7 @@ func encryptFile(key []byte, path string) (err error) {
 	encrypted, _ := encrypt(key, fileContents)
 
 	if err := os.WriteFile(path, encrypted, 0644); err != nil {
-		log.Panicln("Could not overwrite file.")
+		log.Panicln("Could not overwrite cat.")
 	}
 
 	return err
@@ -67,13 +175,13 @@ func encryptFile(key []byte, path string) (err error) {
 func decryptFile(key []byte, path string) (err error) {
 	fileContents, err := os.ReadFile(path)
 	if err != nil || len(path) == 0 {
-		log.Println("File not found.")
+		log.Println("Cat not found.")
 	}
 
 	decrypted, _ := decrypt(key, fileContents)
 
 	if err := os.WriteFile(path, decrypted, 0644); err != nil {
-		log.Panicln("Could not overwrite file.")
+		log.Panicln("Could not overwrite cat.")
 	}
 
 	return err
@@ -93,7 +201,7 @@ func encrypt(key []byte, fileContents []byte) (encrypted []byte, err error) {
 	cipherText := make([]byte, aes.BlockSize+len(fileContents))
 	iv := cipherText[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		log.Panicln("Could not fill iv with random bytes.")
+		log.Panicln("Could not fill iv with dust.")
 	}
 
 	stream := cipher.NewCFBEncrypter(cipherBlock, iv)
